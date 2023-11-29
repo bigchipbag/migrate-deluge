044676214a37355956a710d8543a1852af30df0e
bigchipbag <tbendubois@gmail.com>
2023-11-29 16:23:09 -0700

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,5 @@
-#/bin/bash
-location = $2
-locationEscaped=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<<"$location")
+#!/bin/bash
+location=$2
+#locationEscaped=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<<"$location")
+#no escaping done, should be fixed
 sudo sed -i -E 's|(location)|$2|g' torrents.state
